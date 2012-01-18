2f21393d879b56d486824bdfe95359a575068f8f
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-18 14:30:32 -0500

    Add cron script for problem_db2.

--- modules/ftp/manifests/init.pp
+++ modules/ftp/manifests/init.pp
@@ -6,4 +6,10 @@ class ftp {
         mode   => 0755,
     }
 
+    file { '/etc/cron.daily/update-problem-db2':
+        source => [ "puppet:///modules/ftp/cron/update-problem-db2/$fqdn",
+                    "puppet:///modules/ftp/cron/update-problem-db2/default" ],
+        mode   => 0755,
+    }
+
 }
