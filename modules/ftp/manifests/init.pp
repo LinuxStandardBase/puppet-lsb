15510b44eddf8ffe9812146b515539ef92486522
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-18 13:28:40 -0500

    Fix thinko in FTP module.

--- modules/ftp/manifests/init.pp
+++ modules/ftp/manifests/init.pp
@@ -2,7 +2,7 @@ class ftp {
 
     file { '/etc/cron.daily/update-manifests':
         source => [ "puppet:///modules/ftp/cron/update-manifests/$fqdn",
-                    "puppet:///modules/mysql/cron/update-manifests/default" ],
+                    "puppet:///modules/ftp/cron/update-manifests/default" ],
         mode   => 0755,
     }
 
