f93d5b1f15eae37e22ef1b1e0a88b8557db54bcc
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-19 11:15:47 -0500

    Add initial bzr module.

---
+++ modules/bzr/manifests/init.pp
@@ -0,0 +1,7 @@
+class bzr {
+
+    package {
+        'bzr': ensure => present;
+    }
+
+}
--- modules/ftp/manifests/init.pp
+++ modules/ftp/manifests/init.pp
@@ -1,5 +1,7 @@
 class ftp {
 
+    include bzr
+
     file { '/etc/cron.daily/update-manifests':
         source => [ "puppet:///modules/ftp/cron/update-manifests/$fqdn",
                     "puppet:///modules/ftp/cron/update-manifests/default" ],
