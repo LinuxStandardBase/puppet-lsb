87d453b2ed375b69e0b173f7811e7e4051d1d731
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-22 14:49:32 -0500

    Add script for enabling auto-update of puppet config.

--- modules/puppet/manifests/server.pp
+++ modules/puppet/manifests/server.pp
@@ -7,4 +7,11 @@ class puppet::server inherits puppet {
     # with the default client file.
     File['puppet.conf'] { source => undef }
 
+    # Config auto-update, based on email notifications.
+
+    file { '/usr/local/bin/puppet-email-notify':
+        source => [ "puppet:///modules/puppet/puppet-email-notify" ],
+        mode => 0755,
+    }
+
 }
