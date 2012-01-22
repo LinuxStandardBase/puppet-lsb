1003120a867d36549bbda596ffd1b44c564647d3
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-22 17:37:31 -0500

    Finish puppet auto-update from version control.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,13 @@
 # Top-level Puppet site configuration file.
 
+# Global settings.
+
+Mailalias { notify => Exec['newaliases'] }
+
+exec { newaliases:
+    path => [ '/usr/bin', '/usr/sbin' ],
+    refreshonly => true,
+}
+
 # Import node configuration.
 import 'nodes'
--- modules/puppet/manifests/server.pp
+++ modules/puppet/manifests/server.pp
@@ -8,10 +8,16 @@ class puppet::server inherits puppet {
     File['puppet.conf'] { source => undef }
 
     # Config auto-update, based on email notifications.
+    # Requires that sudo be configured to allow the MTA user
+    # to run "/usr/bin/bzr update -q" as root.
 
     file { '/usr/local/bin/puppet-email-notify':
         source => [ "puppet:///modules/puppet/puppet-email-notify" ],
         mode => 0755,
+    }
+
+    mailalias ( 'puppet-notify':
+        recipient => '|/usr/local/bin/puppet-email-notify',
     }
 
 }
