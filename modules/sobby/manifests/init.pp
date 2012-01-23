aed060c395eb3365a00b1e0dd63b37e83213906e
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-22 20:10:17 -0500

    Add service restarts on file changes for sobby, and ensure it's running.

--- modules/sobby/manifests/init.pp
+++ modules/sobby/manifests/init.pp
@@ -4,15 +4,17 @@ class sobby {
 
     file { "/etc/sobby.conf":
         source => "puppet:///modules/sobby/sobby.conf",
+        notify => Service['sobby'],
     }
 
     file { "/etc/init.d/sobby":
         source => "puppet:///modules/sobby/sobby.init",
         mode   => 0755,
+        notify => Service['sobby'],
     }
 
     service { "sobby":
-        #ensure     => running,
+        ensure     => running,
         hasrestart => false,
         hasstatus  => false,
     }
