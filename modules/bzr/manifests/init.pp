593a33067cfe1ee8b1dbb0ed6e1c11597e37de28
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-23 11:33:33 -0500

    Add SLES 11 repo for updated bzr.

--- modules/bzr/manifests/init.pp
+++ modules/bzr/manifests/init.pp
@@ -4,4 +4,26 @@ class bzr {
         'bzr': ensure => present;
     }
 
+    # bzr on SLES 11 is too old; add a repository on the openSUSE
+    # Build Service for newer bzr.
+
+    $dosles11obs = "$operatingsystem-$operatingsystemrelease" ? {
+        /^SLES-11(\.[0-9])?$/ => false,
+        default               => true,
+    }
+
+    file { "/etc/zypp/repos.d/devel_tools_scm.repo":
+        noop   => $dosles11obs,
+        source => "puppet:///modules/bzr/devel_tools_scm.repo",
+        notify => Exec['refresh-zypper-keys'],
+        before => Package['bzr'],
+    }
+
+    exec { 'refresh-zypper-keys':
+        command     => 'zypper --gpg-auto-import-keys refresh',
+        path        => [ '/usr/sbin', '/usr/bin', '/bin', '/sbin' ],
+        refreshonly => true,
+        logoutput   => true,
+    }
+
 }
