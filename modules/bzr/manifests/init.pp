b279eb519328b4ec118256c9f4175132d7a815a2
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-25 17:21:11 -0500

    Apply fixes from virtualenv module to bzr module.

--- modules/bzr/manifests/init.pp
+++ modules/bzr/manifests/init.pp
@@ -1,25 +1,26 @@
 class bzr {
 
-    package {
-        'bzr': ensure => present;
-    }
-
     # bzr on SLES 11 is too old; add a repository on the openSUSE
     # Build Service for newer bzr.
 
-    $skipsles11obs = "$operatingsystem-$operatingsystemrelease" ? {
-        /^SLES-11(\.[0-9])?$/ => false,
-        default               => true,
+    $sles11obsrepo = "$operatingsystem-$operatingsystemrelease" ? {
+        /^SLES-11(\.[0-9])?$/ => File['/etc/zypp/repos.d/devel_tools_scm.repo'],
+        default               => undef,
+    }
+
+    package {
+        'bzr': ensure => present;
+        require => $sles11obsrepo,
     }
 
     file { "/etc/zypp/repos.d/devel_tools_scm.repo":
         noop   => $skipsles11obs,
         source => "puppet:///modules/bzr/devel_tools_scm.repo",
-        notify => Exec['refresh-zypper-keys'],
+        notify => Exec['refresh-zypper-keys-for-bzr'],
         before => Package['bzr'],
     }
 
-    exec { 'refresh-zypper-keys':
+    exec { 'refresh-zypper-keys-for-bzr':
         command     => 'zypper --gpg-auto-import-keys refresh',
         path        => [ '/usr/sbin', '/usr/bin', '/bin', '/sbin' ],
         refreshonly => true,
