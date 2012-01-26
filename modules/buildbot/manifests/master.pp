001aba9a4c3d99a2f018a9bc80cd389bdc10ea7d
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-25 21:54:17 -0500

    Set up buildbot master configuration.

--- modules/buildbot/manifests/master.pp
+++ modules/buildbot/manifests/master.pp
@@ -8,18 +8,26 @@ class buildbot::master inherits buildbot {
         require => File["/opt/buildbot/Makefile"],
     }
 
-    exec { "make buildbot-config":
+    exec { "make-buildbot-config":
+        command => "make buildbot-config",
         cwd     => "/opt/buildbot",
         creates => "/opt/buildbot/buildbot-config",
         path    => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
         require => File["/opt/buildbot/Makefile"],
     }
 
-    exec { "/opt/buildbot/bin/buildbot create-master /opt/buildbot/lsb-master":
+    exec { "make-master":
+        command => "/opt/buildbot/bin/buildbot create-master /opt/buildbot/lsb-master",
         cwd     => "/opt/buildbot",
         creates => "/opt/buildbot/lsb-master",
         path    => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
         require => Exec["make-buildbot"],
+    }
+
+    file { "/opt/buildbot/lsb-master/master.cfg":
+        ensure  => link,
+        target  => "../buildbot-config/lsb_master.cfg",
+        require => [ Exec['make-buildbot-config'], Exec['make-master'] ],
     }
 
 }
