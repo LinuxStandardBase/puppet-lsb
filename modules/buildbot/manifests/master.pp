06c602faec25dc23ef4187f47e7b8341e57bde5e
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-25 21:47:26 -0500

    Express the master directory dependency differently.

--- modules/buildbot/manifests/master.pp
+++ modules/buildbot/manifests/master.pp
@@ -1,14 +1,15 @@
 class buildbot::master inherits buildbot {
 
-    exec { "make bin/buildbot":
-        cwd => "/opt/buildbot",
+    exec { "make-buildbot":
+        command => "make bin/buildbot",
+        cwd     => "/opt/buildbot",
         creates => "/opt/buildbot/bin/buildbot",
         path    => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
         require => File["/opt/buildbot/Makefile"],
     }
 
     exec { "make buildbot-config":
-        cwd => "/opt/buildbot",
+        cwd     => "/opt/buildbot",
         creates => "/opt/buildbot/buildbot-config",
         path    => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
         require => File["/opt/buildbot/Makefile"],
@@ -18,7 +19,7 @@ class buildbot::master inherits buildbot {
         cwd     => "/opt/buildbot",
         creates => "/opt/buildbot/lsb-master",
         path    => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
-        require => File["/opt/buildbot/bin/buildbot"],
+        require => Exec["make-buildbot"],
     }
 
 }
