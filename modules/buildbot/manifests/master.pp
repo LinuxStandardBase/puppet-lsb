8fa6b761a0f9c314237837b89d5d1849b8b63c77
Jeff Licquia <licquia@linuxfoundation.org>
2012-01-25 22:01:16 -0500

    Create other master symlinks needed.

--- modules/buildbot/manifests/master.pp
+++ modules/buildbot/manifests/master.pp
@@ -30,4 +30,16 @@ class buildbot::master inherits buildbot {
         require => [ Exec['make-buildbot-config'], Exec['make-master'] ],
     }
 
+    file { "/opt/buildbot/lsb-master/bzr_buildbot.py":
+        ensure  => link,
+        target  => "../buildbot-config/bzr_buildbot.py",
+        require => [ Exec['make-buildbot-config'], Exec['make-master'] ],
+    }
+
+    file { "/opt/buildbot/lsb-master/lfbuildbot.py":
+        ensure  => link,
+        target  => "../buildbot-config/lfbuildbot.py",
+        require => [ Exec['make-buildbot-config'], Exec['make-master'] ],
+    }
+
 }
