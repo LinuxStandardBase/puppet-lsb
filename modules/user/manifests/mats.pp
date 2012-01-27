3e85b823562d5290f3cc67dd299cd9b38f983b93
Mats Wichmann <mats@linuxfoundation.org>
2012-01-27 11:48:44 -0700

    add key for mats

--- modules/user/manifests/mats.pp
+++ modules/user/manifests/mats.pp
@@ -12,13 +12,8 @@ class user::mats inherits user::virtual {
     ssh_authorized_key { 'mats@linuxfoundation.org':
         ensure  => present,
         user    => 'mats',
-        # Pick the correct type (first word in the public key file).
         type    => 'ssh-rsa',
-        #type    => 'ssh-dss',
-        # Paste key between the quotes (the gobbledygook w/o the type or id)
-        key     => '',
-        # Delete the following line once the ssh key has been added.
-        noop    => true,
+        key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDCLtvB4thaICbb/smVwnDeCOrcVjyWmU7mqzQrUvk+vhhWa/eQknVnQIu035PoK5/luaVJZypv2s4rN31ckhi1QPhej9paM83ANYdzwvzJ7IAZomztRhnbqGlsVLSbl0nnym4FV1iv/pIbp0UcbDyY+0dQ3pe5Noo1+oMAoxv/bDRk4YwTeEBv1H7Us+7aDXgerWTOLS07ck3OC5Acg9nQ0SUwDh/YmW0IkZMLjlH1INIEoYaNoXUI7ujdlNdk6ojUH8R1KiP2kaJeIN1j6Jd1RK5zJ40rdvq/NUDPuht42eGV8oE/Nf9wVL+AFJJexGWaFgjlHt2mBe6ENbkr6dsj',
     }
 
 }
