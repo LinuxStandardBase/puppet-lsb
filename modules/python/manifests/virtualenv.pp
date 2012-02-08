745ecd928dcb20a107dbe2ba6137ca0643040b15
Jeff Licquia <licquia@linuxfoundation.org>
2012-02-08 18:51:09 -0500

    Try specifying the repo files for Python a different way.

--- modules/python/manifests/virtualenv.pp
+++ modules/python/manifests/virtualenv.pp
@@ -1,27 +1,34 @@
 class python::virtualenv inherits python {
 
-    # These packages aren't available in SLES 11, so we'll need
+    # These packages aren't available in certain SUSE variants, so we'll need
     # to add the repos first.
 
-    $sles11obsrepo = "$operatingsystem-$operatingsystemrelease" ? {
-        /^SLES-11(\.[0-9])?$/ => File['/etc/zypp/repos.d/devel_languages_python_sles11.repo'],
-        /^OpenSuSE-11\.4$/    => File['/etc/zypp/repos.d/devel_languages_python_opensuse11.repo'],
+    $osid = "$operatingsystem-$operatingsystemrelease"
+
+    $suseobsrepo = "$osid" ? {
+        /^SLES-11(\.[0-9])?$/ => File['/etc/zypp/repos.d/devel_languages_python.repo'],
+        /^OpenSuSE-11\.4$/    => File['/etc/zypp/repos.d/devel_languages_python.repo'],
         default               => undef,
+    }
+
+    $susereposrc = "$osid" ? {
+        /^SLES-11(\.[0-9])?$/ => "devel_languages_python_sles11.repo",
+        /^OpenSuSE-11\.4$/    => "devel_languages_python_opensuse11.repo",
     }
 
     package { 'python-virtualenv': 
         ensure  => present,
-        require => $sles11obsrepo,
+        require => $suseobsrepo,
     }
 
     package { 'python-pip':
         ensure => present,
-        require => $sles11obsrepo,
+        require => $suseobsrepo,
     }
 
-    if $sles11obsrepo {
+    if $suseobsrepo {
         file { "/etc/zypp/repos.d/devel_languages_python.repo":
-            source => "puppet:///modules/python/devel_languages_python.repo",
+            source => "puppet:///modules/python/$susereposrc",
             notify => Exec['refresh-zypper-keys-for-python'],
         }
 
