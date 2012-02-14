2304150da480c2ee4d1f194e3e5b56164e3fe903
Jeff Licquia <licquia@linuxfoundation.org>
2012-02-14 16:28:53 -0500

    Redirect buildbot links to the proper place for link resolution.
    Also, when vhost changes are made, reload the web service.

--- modules/apachehttpd/manifests/vhosts.pp
+++ modules/apachehttpd/manifests/vhosts.pp
@@ -1,13 +1,14 @@
 class apachehttpd::vhosts inherits apachehttpd {
 
     file { '/etc/apache2/vhosts.d':
-        ensure => directory,
-        source => "puppet:///modules/apachehttpd/vhosts.d/$fqdn",
-        recurse => true,
+        ensure       => directory,
+        source       => "puppet:///modules/apachehttpd/vhosts.d/$fqdn",
+        recurse      => true,
         recurselimit => 1,
-        links => follow,
-        purge => true,
-        require => Package['apache2'],
+        links        => follow,
+        purge        => true,
+        require      => Package['apache2'],
+        notify       => Service['apache2'],
     }
 
 }
