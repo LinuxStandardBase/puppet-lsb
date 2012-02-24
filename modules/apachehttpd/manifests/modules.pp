f71ff59dbf3646e2f1b18b05b5d82de28379e882
Jeff Licquia <licquia@linuxfoundation.org>
2012-02-24 17:54:28 -0500

    Add path settings for all bzr invocations.

--- modules/apachehttpd/manifests/modules.pp
+++ modules/apachehttpd/manifests/modules.pp
@@ -6,31 +6,37 @@ class apachehttpd::modules {
 
     exec { 'make-dbadmin-module':
         command => "cd /srv/www/modules && bzr checkout -r $dbadminrev http://bzr.linuxfoundation.org/lsb/devel/dbadmin",
+        path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
         creates => '/srv/www/modules/dbadmin',
     }
 
     exec { 'make-lsbcert-module':
         command => "cd /srv/www/modules && bzr checkout -r $certrev http://bzr.linuxfoundation.org/lsb/devel/lsb-cert",
+        path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
         creates => '/srv/www/modules/lsb-cert',
     }
 
     exec { 'make-prdb-module':
         command => "cd /srv/www/modules && bzr checkout -r $prdbrev http://bzr.linuxfoundation.org/lsb/devel/prdb",
+        path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
         creates => '/srv/www/modules/prdb',
     }
 
     exec { 'update-dbadmin-module':
         command => "bzr update -r $dbadminrev /srv/www/modules/dbadmin",
+        path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
         require => Exec['make-dbadmin-module'],
     }
 
     exec { 'update-lsbcert-module':
         command => "bzr update -r $certrev /srv/www/modules/lsb-cert",
+        path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
         require => Exec['make-lsbcert-module'],
     }
 
     exec { 'update-prdb-module':
         command => "bzr update -r $prdbrev /srv/www/modules/prdb",
+        path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
         require => Exec['make-prdb-module'],
     }
 
