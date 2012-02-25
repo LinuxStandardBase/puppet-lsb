4107f6120ed659eb314658170de9871bc9886cef
Jeff Licquia <licquia@linuxfoundation.org>
2012-02-24 20:47:52 -0500

    Manage config files for web databases via Puppet.

--- modules/apachehttpd/manifests/modules.pp
+++ modules/apachehttpd/manifests/modules.pp
@@ -1,8 +1,18 @@
 class apachehttpd::modules {
 
+    # Pull in web database passwords.  These are actually used in the
+    # db config templates.
+
+    include webdb
+
+    # Revisions we want for each module.  This is just passed to -r,
+    # so any bzr revisionspec is allowed (revnos, tags, revids, etc.).
+
     $dbadminrev = 'revid:licquia@linuxfoundation.org-20120222174745-komaytu3rpkmyd5k'
     $certrev = 'revid:licquia@licquia.org-20111228203501-46t7fzgrf3q2166n'
     $prdbrev = 'revid:stewb@linux-foundation.org-20110413225941-02hl9ije28ol0qc6'
+
+    # Do initial checkouts of modules.
 
     exec { 'make-dbadmin-module':
         command => "cd /srv/www/modules && bzr checkout -r $dbadminrev http://bzr.linuxfoundation.org/lsb/devel/dbadmin",
@@ -21,6 +31,22 @@ class apachehttpd::modules {
         path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
         creates => '/srv/www/modules/prdb',
     }
+
+    # Set module configuration files.
+
+    file { '/srv/www/modules/dbadmin/config/connection.inc':
+        content => template('apachehttpd/dbconfig/dbadmin.erb'),
+    }
+
+    file { '/srv/www/modules/lsb-cert/config.inc.php':
+        content => template('apachehttpd/dbconfig/lsb-cert.erb'),
+    }
+
+    file { '/srv/www/modules/prdb/config.inc.php':
+        content => template('apachehttpd/dbconfig/prdb.erb'),
+    }
+
+    # Check for and pull updates for modules.
 
     exec { 'update-dbadmin-module':
         command => "bzr update -r $dbadminrev /srv/www/modules/dbadmin",
