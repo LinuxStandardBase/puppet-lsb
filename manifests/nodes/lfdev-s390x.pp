1138dfa0190df95c813a8e2b4a1001bc091d98ca
Jeff Licquia <licquia@linuxfoundation.org>
2012-03-15 15:35:57 -0400

    Add cron jobs for weekly and monthly work reports on lfdev-s390x.

--- manifests/nodes/lfdev-s390x.pp
+++ manifests/nodes/lfdev-s390x.pp
@@ -8,4 +8,20 @@ node 'etpglr3.dal-ebis.ihost.com' {
 
     include buildbot::slavechroot
 
+    # These are special entries for monitoring workload on the IBM server.
+
+    cron { 's390x-weekly-work-report':
+        command => '/opt/wureport/wureport | mail -s "Weekly work report for lfdev-s390x" licquia@linuxfoundation.org',
+        hour    => '20',
+        minute  => '0',
+        weekday => 'Sunday',
+    }
+
+    cron { 's390x-monthly-work-report':
+        command  => '/opt/wureport/wureport $(date -d "1 month ago" +%m/%d/%y) $(date +%m/%d/%y) | mail -s "Monthly work report for lfdev-s390x" licquia@linuxfoundation.org',
+        hour     => '5',
+        minute   => '0',
+        monthday => '1',
+    }
+
 }
