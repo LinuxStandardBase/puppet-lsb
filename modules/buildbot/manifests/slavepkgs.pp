7f94f3a74bef7973c627a595d076d84b61a50202
Jeff Licquia <licquia@linuxfoundation.org>
2012-03-23 21:56:24 -0400

    Switch Java package on ia64; IBM Java is too old.

--- modules/buildbot/manifests/slavepkgs.pp
+++ modules/buildbot/manifests/slavepkgs.pp
@@ -49,7 +49,7 @@ class buildbot::slavepkgs {
         /^SLES-x86_64$/ => 'java-1_6_0-ibm',
         /^SLES-s390x$/  => 'java-1_6_0-ibm',
         /^SLES-ppc64$/  => 'java-1_6_0-ibm',
-        /^SLES-ia64$/   => 'java-1_4_2-ibm',
+        /^SLES-ia64$/   => 'java-1_5_0-bea',
         /^CentOS/ => 'java-1.6.0-openjdk',
         default   => 'openjdk',
     }
