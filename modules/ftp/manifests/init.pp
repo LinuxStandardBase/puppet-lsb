4ebcf1bc474f291617df71aeb9489a45e729ef6f
Jeff Licquia <licquia@linuxfoundation.org>
2013-04-04 08:34:38 -0400

    Wrong cron syntax.

--- modules/ftp/manifests/init.pp
+++ modules/ftp/manifests/init.pp
@@ -78,7 +78,7 @@ class ftp {
     cron { 'regular-update-problem-db2':
         command => '/usr/local/bin/update-problem-db2',
         user    => root,
-        hour    => '3,6,9,12,15,18,21',
+        hour    => [ 3, 6, 9, 12, 15, 18, 21 ],
         minute  => 37,
     }
 
