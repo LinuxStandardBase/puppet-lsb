1fa6d3ca0bbb0df4c8a15acc6d34152fd9b25b79
Jeff Licquia <licquia@linuxfoundation.org>
2013-04-16 09:14:23 -0700

    Push buildbot fix.

--- modules/buildbot/manifests/master.pp
+++ modules/buildbot/manifests/master.pp
@@ -4,7 +4,7 @@ class buildbot::master inherits buildbot {
 
     include buildbotpw
 
-    $buildbotconfigrev = 'revid:licquia@linuxfoundation.org-20130416055350-9zk0e7sdhj74xn42'
+    $buildbotconfigrev = 'revid:licquia@linuxfoundation.org-20130416161154-e82rbzdp3dqavlq0'
 
     $weeklyrebuildarchs = 'x86,x86_64,ia64,ppc32,ppc64'
 
