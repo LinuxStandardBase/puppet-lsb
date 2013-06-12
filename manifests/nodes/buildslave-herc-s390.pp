ad6740ab6331ba8b9699e5f8ed2057c2bce39fd5
Jeff Licquia <licquia@linuxfoundation.org>
2013-06-11 22:23:49 -0400

    Add backup s390 build slaves running under Hercules to puppet.

---
+++ manifests/nodes/buildslave-herc-s390.pp
@@ -0,0 +1,10 @@
+# Backup 31-bit s390 build slave on Hercules.
+node 'buildslave-s390' {
+
+    include sudo
+
+    include puppet
+
+    #include buildbot::slave
+
+}
---
+++ manifests/nodes/buildslave-herc-s390x.pp
@@ -0,0 +1,10 @@
+# Backup 64-bit s390 build slave on Hercules.
+node 'buildslave-s390x' {
+
+    include sudo
+
+    include puppet
+
+    #include buildbot::slave
+
+}
