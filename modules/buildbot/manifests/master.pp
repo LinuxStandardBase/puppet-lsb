741faf0010458b8bb6e598b9ee1c94aa536f03bc
Jeff Licquia <licquia@linuxfoundation.org>
2013-07-07 12:44:30 -0400

    Bump buildbot config.

--- modules/buildbot/manifests/master.pp
+++ modules/buildbot/manifests/master.pp
@@ -4,7 +4,7 @@ class buildbot::master inherits buildbot {
 
     include buildbotpw
 
-    $buildbotconfigrev = 'revid:licquia@linuxfoundation.org-20130423141055-0em1p71q0hrd5php'
+    $buildbotconfigrev = 'revid:licquia@linuxfoundation.org-20130707164248-d6roleeix4tsyrdc'
 
     $weeklyrebuildarchs = 'x86,x86_64,ia64,ppc32,ppc64'
 
