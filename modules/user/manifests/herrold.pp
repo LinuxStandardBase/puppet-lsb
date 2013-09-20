cf6b6e78f35c9de20cbfa5bcefcd8e15729d6b2d
Jeff Licquia <licquia@linuxfoundation.org>
2013-09-20 09:54:20 -0400

    Correct errors in the SSH key definition for Russ.

--- modules/user/manifests/herrold.pp
+++ modules/user/manifests/herrold.pp
@@ -10,10 +10,10 @@ class user::herrold inherits user::virtual {
     }
 
     # No SSH key yet! -- delete this line once we get a successful commit at the master
-    ssh_authorized_key { 'herrold@owlriver.com':
+    ssh_authorized_key { 'herrold+lf-2013-rsa@owlriver.com':
         ensure  => present,
         type    => 'ssh-rsa',
-        key     => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA7q203HceYQBUBzJSpp6IP0aIfH8PLLbqYdRM38u5X/W0P+r2gYIq7hr2R4+wYvCZvySXoA3dukrI/V/bTsF5W++h2LTtFNZvSTZYbv/ZDFfe9QlOEamY+kKZkgvOByZiOJhv4jnxIY1IUmSQHdy0CWjA82VMncaTm3vOkg+565SIhFAgxLxu/UhH7IK66IEreefAx5qFVF4O6jU3kKKzUZaimqXwIJuZvce52l8qc6zZyLobuEqTPv5mwLSH8LbZKlf/N5jWJJ1Ou1p8tZB4BZbeJv8FP4cHVMyHf9EJUkKKdeQiI2N8X8c4zy+Kqc/+kXf5I1UcwyOsLAFOTlfQsw== herrold+lf-2013-rsa@owlriver.com',
+        key     => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA7q203HceYQBUBzJSpp6IP0aIfH8PLLbqYdRM38u5X/W0P+r2gYIq7hr2R4+wYvCZvySXoA3dukrI/V/bTsF5W++h2LTtFNZvSTZYbv/ZDFfe9QlOEamY+kKZkgvOByZiOJhv4jnxIY1IUmSQHdy0CWjA82VMncaTm3vOkg+565SIhFAgxLxu/UhH7IK66IEreefAx5qFVF4O6jU3kKKzUZaimqXwIJuZvce52l8qc6zZyLobuEqTPv5mwLSH8LbZKlf/N5jWJJ1Ou1p8tZB4BZbeJv8FP4cHVMyHf9EJUkKKdeQiI2N8X8c4zy+Kqc/+kXf5I1UcwyOsLAFOTlfQsw==',
         user    => 'herrold',
     }
 
