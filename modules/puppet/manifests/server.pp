ed6a5daa6b483fb3ba1d4865a4cac08bde9d0c34
Jeff Licquia <licquia@linuxfoundation.org>
2014-02-20 20:45:40 -0500

    Remove special repo for puppet master recipe.

--- modules/puppet/manifests/server.pp
+++ modules/puppet/manifests/server.pp
@@ -2,7 +2,6 @@ class puppet::server inherits puppet {
 
     package { 'puppet-server':
         ensure  => $puppetmasterversion,
-        require => File['/etc/zypp/repos.d/systemsmanagement_puppet.repo'],
     }
 
     # For the Puppet server, puppet.conf is in the root of
