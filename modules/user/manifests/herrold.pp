b527e3b359f9cc4b60c55b428ca5ec00947e388b
Jeff Licquia <licquia@linuxfoundation.org>
2014-02-28 12:49:27 -0500

    Fix Russ's SSH key entry.

--- modules/user/manifests/herrold.pp
+++ modules/user/manifests/herrold.pp
@@ -14,7 +14,7 @@ class user::herrold inherits user::virtual {
         ensure  => present,
         type    => 'ssh-rsa',
         key     => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAuBgHhm6RzKd2MUJLv2G36PVi7fgH+haefHQRH1ZCPv4FHbDuKLHJr2T6JrrzUCPnlX7wXNGTJPlAmiw8mfM4eTdgybjXE4Htj9CewyPP8hzOSc3bxQD4UgdPGy0XsxHQg3weAg1+/K5iXpYMLsrsF2yub9xZ9+0FuXFMkVq2W0AbOoUF+2DX5e461RfZkFxtUjcKZNHgFtPhwzcVMmQVj+96m28jJFnDCWJI5vcUoMIf3VnH4XX757jNOj+KX87eE7q4hUhkn7GZ0k2/nkDKsh3Ey01IvxLsjquf4wVN5T3ESk3EAgtSlq6XfbK90POZqtYIz5g/bDT8QOyRu9SMjw==',
-        user    => 'herrold+lf-2014-rsa@owlriver.com',
+        user    => 'herrold',
     }
 
 
