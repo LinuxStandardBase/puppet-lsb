6396997db4278f4d33e691cd4b498b3cc610b5a6
Jeff Licquia <licquia@linuxfoundation.org>
2014-04-10 10:34:56 -0400

    Fix minor content glitch in TLS config.

--- modules/apachehttpd/manifests/ssl.pp
+++ modules/apachehttpd/manifests/ssl.pp
@@ -3,7 +3,7 @@ class apachehttpd::ssl {
     include tls
 
     file { '/etc/apache2/ssl.crt/linuxbase-sf_bundle.pem':
-        content => "${tls::cert}\n${tls::sf_bundle_g2}",
+        content => "${tls::cert}${tls::sf_bundle_g2}",
     }
 
     file { '/etc/apache2/ssl.key/linuxbase.key':
