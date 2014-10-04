d1a3a8591f0ab71cf4cda4778c9aa745d008b2e4
Jeff Licquia <licquia@linuxfoundation.org>
2014-10-04 17:00:38 -0400

    Oops, looked in wrong place.  Reverting.

--- manifests/nodes/lsb2.pp
+++ manifests/nodes/lsb2.pp
@@ -10,9 +10,8 @@ node 'lsb2.linux-foundation.org' {
 
     include mail::linuxbase
 
-    include apachehttpd, apachehttpd::php, apachehttpd::vhosts,
-            apachehttpd::betaspecs, apachehttpd::modules, apachehttpd::ssl,
-            apachehttpd::linuxbase
+    include apachehttpd, apachehttpd::vhosts, apachehttpd::betaspecs,
+            apachehttpd::modules, apachehttpd::ssl, apachehttpd::linuxbase
 
     include php
 
