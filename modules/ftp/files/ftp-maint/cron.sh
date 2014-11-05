8246a90a02da5908557880e7d2fe9e653043538d
Jeff Licquia <licquia@linuxfoundation.org>
2014-11-05 10:51:58 -0500

    Remove old manifest generation for dist-checker.

--- modules/ftp/files/ftp-maint/cron.sh
+++ modules/ftp/files/ftp-maint/cron.sh
@@ -2,14 +2,8 @@
 
 MAINT_SCRIPTS_HOME=/opt/ftp-maint
 
-cd $MAINT_SCRIPTS_HOME/manifest-old && ./manifest_rebuild_s.sh >cronmsg 2>&1
-grep -i '\(warning\|error\)' cronmsg >/dev/null 2>&1 && echo -e "\n\n--- Manifest-old rebuild ---\n\n" && cat cronmsg
-
 cd $MAINT_SCRIPTS_HOME/manifest && ./manifest_rebuild_s.sh >cronmsg 2>&1
 grep -i '\(warning\|error\)' cronmsg >/dev/null 2>&1 && echo -e "\n\n--- Manifest rebuild ---\n\n" && cat cronmsg
-
-#cd $MAINT_SCRIPTS_HOME/manifest && ./manifest_rebuild_moblin.sh >cronmsg 2>&1
-#grep -i '\(warning\|error\)' cronmsg >/dev/null 2>&1 && echo -e "\n\n--- Manifest for Moblin ---\n\n" && cat cronmsg
 
 $MAINT_SCRIPTS_HOME/problem_db_update.sh >cronmsg 2>&1
 grep -i '\(warning\|error\)' cronmsg >/dev/null 2>&1 && echo -e "\n\n--- problem_db update ---\n\n" && cat cronmsg
