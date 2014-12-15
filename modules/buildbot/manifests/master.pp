e369d50b65424a847a5b3daeeba034239ba55122
Jeff Licquia <licquia@linuxfoundation.org>
2014-12-15 11:14:17 -0500

    Bump buildbot-config rev.

--- modules/buildbot/manifests/master.pp
+++ modules/buildbot/manifests/master.pp
@@ -6,7 +6,7 @@ class buildbot::master inherits buildbot {
 
     $sqlalchemyversion = '0.7.10'
 
-    $buildbotconfigrev = 'revid:licquia@linuxfoundation.org-20140930215032-2hynqh577s0vw0hr'
+    $buildbotconfigrev = 'revid:licquia@linuxfoundation.org-20141215161109-u2u6nk1l9l06vfs7'
 
     $weeklyrebuildarchs = 'x86,x86_64,ia64,ppc32,ppc64,s390,s390x'
 
