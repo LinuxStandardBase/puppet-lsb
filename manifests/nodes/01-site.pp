54cd7b971125ef0e1aafd2fde6e605abeeaadfd7
Jeff Licquia <licquia@linuxfoundation.org>
2018-05-09 10:01:03 -0400

    Start migration from Puppet 3 to Puppet 4.

---
+++ manifests/nodes/01-site.pp
@@ -0,0 +1,11 @@
+# Top-level Puppet site configuration file.
+
+# Global settings.
+
+Mailalias { notify => Exec['newaliases'] }
+
+exec { newaliases:
+    path => [ '/usr/bin', '/usr/sbin' ],
+    refreshonly => true,
+}
+
