0b3ec7a960dc01ee3858951eeef7bad0f73f2e57
Jeff Licquia <licquia@linuxfoundation.org>
2018-09-28 09:42:28 -0400

    Add vm3.linuxbase.org node to Puppet.

---
+++ manifests/nodes/vm3.pp
@@ -0,0 +1,15 @@
+node 'vm3.linuxbase.org' {
+
+    include puppet
+
+    include user::lfadmin, user::licquia, user::mats
+
+    include ntp
+
+    include sudo
+
+    include ldap
+
+    include alien
+
+}
