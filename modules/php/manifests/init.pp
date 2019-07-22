7726852708619417c6e890cb364693d6ea4eb6e7
Jeff Licquia <licquia@linuxfoundation.org>
2019-07-21 22:09:39 -0400

    Fix PHP setup on RHEL/CentOS 7.

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -2,7 +2,8 @@ class php {
 
     $osdefault = "${operatingsystem}-${operatingsystemrelease}" ? {
         /^SLES-11/       => 'default-sles11',
-        /^OpenSuSE-13/ => 'default-opensuse',
+        /^OpenSuSE-13/   => 'default-opensuse',
+        /^CentOS-7/      => 'default-rhel7',
     }
 
     case $operatingsystem {
@@ -20,6 +21,20 @@ class php {
                 source  => [ "puppet:///modules/php/php.ini/$fqdn",
                              "puppet:///modules/php/php.ini/$osdefault" ],
                 require => Package['apache2-mod_php5'],
+            }
+        }
+
+        /^(CentOS|RHEL)$/: {
+            package {
+                'php': ensure => installed;
+                'php-ldap': ensure => installed;
+                'php-mysql': ensure => installed;
+            }
+
+            file { '/etc/php.ini':
+                source  => [ "puppet:///modules/php/php.ini/$fqdn",
+                             "puppet:///modules/php/php.ini/$osdefault" ],
+                require => Package['php'],
             }
         }
     }
